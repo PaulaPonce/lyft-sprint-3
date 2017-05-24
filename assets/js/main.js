2c8ead1c7937ca9162ff0dbf0fdfcec8a5f1a8bc
Paula <paula.poncez@usach.cl>
2017-05-24 11:23:21 -0400

    Tablero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 //validate phone number
 var driveBtn = document.getElementById("drive");
 driveBtn.addEventListener("click", validatePhone);
@@ -18,10 +17,42 @@ function validateCoord(){
 	var xCoord = document.getElementById("x-coord").value;
 	var yCoord = document.getElementById("y-coord").value;
 	if(xCoord > 10){
-		alert("X Coordinate < 10");
+		alert("0 < X Coordinate < 10");
 	}
-	if(yCoord > 6){
-		alert("X Coordinate < 6");
+	else if(yCoord > 6){
+		alert("0 < X Coordinate < 6");
+	}else{
+		//voy a guardar el tablero en el div "lyft-game"
+		var lyftGame = document.getElementsByClassName("lyft-game");
+		var contBoard = document.createElement("div");
+		contBoard.setAttribute("id","board");
+		lyftGame.appendChild(contBoard);
+
+		//board
+		var board = [
+		  [0,0,0,0,0,0,0,0,0,0],
+		  [0,0,0,0,0,0,0,0,0,0],
+		  [0,0,0,0,0,0,0,0,0,0],
+		  [0,0,0,0,0,0,0,0,0,0],
+		  [0,0,0,0,0,0,0,0,0,0],
+		  [0,0,0,0,0,0,0,0,0,0]
+		];
+
+		var divBoard = document.getElementById("board");
+		var row, column;
+
+		for(var i = 0; i < board.length; i++){
+			row = document.createElement("div");
+			row.classList.add("row");
+			for(var j = 0; j < board[i].length; j++){
+				column = document.createElement("div");
+				column.innerHTML = board[i][j];
+				column.classList.add("column");
+				row.appendChild(column);
+			}
+			divBoard.appendChild(row);
+			contBoard.appendChild(divBoard);
+		}
 	}
 }
 
@@ -34,3 +65,24 @@ function showAlert(){
 	};
 }
 showAlert();
+
+//Play Lyft
+function Auto(posicion_x, posicion_y){
+	this.posicion_x = posicion_x;
+	this.posicion_y = posicion_y;
+
+	this.avanzar = function(){
+		this.posicion_y = this.posicion_y + 1;
+	};
+	this.ir_izquierda = function(){
+		this.posicion_x = this.posicion_x - 1;
+	};
+	this.ir_derecha = function(){
+		this.posicion_x = this.posicion_x + 1;
+	};
+	this.retroceder = function(){
+		this.posicion_y = this.posicion_y - 1;
+	}
+}
+
+
